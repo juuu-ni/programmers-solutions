9d9f32c55882104d73d81f278a40e3b275e9432a
juuu-ni <ljh23502@naver.com>
2026-05-10 23:30:06 +0900

    이상한 문자 만들기

---
+++ programmers/level1/5.10/Make strange characters.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer=[]
+    words = s.split(" ")
+    for word in words:
+        new_word =""
+        for i in range(len(word)):
+            if i%2==0:
+                new_word+=word[i].upper()
+            else:
+                new_word+=word[i].lower()
+            
+        answer.append(new_word)
+    return ' '.join(answer)

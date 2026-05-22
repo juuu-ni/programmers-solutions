849b23a2a7e2c0bee9347bd9c9df4b3fe14f9902
juuu-ni <ljh23502@naver.com>
2026-05-22 17:29:33 +0900

    백만장자 프로젝트

---
+++ programmers/level1/5.22/Two_secret.py
@@ -0,0 +1,9 @@
+def solution(s, skip, index):
+    alpha = [i for i in 'abcdefghijklmnopqrstuvwxyz' if i not in skip]
+    result = ''
+    
+    for a in s:
+        answer = alpha.index(a)
+        result +=alpha[(answer + index)%len(alpha)]
+            
+    return result
---
+++ programmers/level1/5.22/[SWEA]Millionaire.py
@@ -0,0 +1,16 @@
+T = int(input())
+for t in range(T):
+    day = int(input())
+    prices = list(map(int,input().split()))
+
+    max_price = 0
+    total = 0
+
+    for price in reversed(prices):
+        if price> max_price:
+            max_price = price
+    
+        else:
+            total += max_price - price
+
+    print(f"#{t+1} {total}")

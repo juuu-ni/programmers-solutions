615d730804e3f1f595d8bdfb2ec4cae7ca8a1d48
juuu-ni <ljh23502@naver.com>
2026-05-22 18:13:43 +0900

    [SWEA]파리 퇴치

--- programmers/level1/5.22/[SWEA]FlyKiller.py
+++ programmers/level1/5.22/[SWEA]FlyKiller.py
@@ -3,6 +3,7 @@ for test_case in range(1, T + 1):
     N, M = map(int, input().split())
     array = [list(map(int, input().split())) for _ in range(N)]
      
+     
     mx = 0
     for i in range(N - M + 1):
         for j in range(N - M + 1):
